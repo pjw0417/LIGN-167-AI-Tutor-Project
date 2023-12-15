f26ba7d5e897ecef905b70ee23ec3a5bec028804
Jungwoo Park <114387488+pjw0417@users.noreply.github.com>
2023-12-15 03:17:02 -0800

    added feedback for cheatsheet generetor

--- cheat_sheet_generator.js
+++ cheat_sheet_generator.js
@@ -21,21 +21,42 @@ const inputFuture= document.getElementById("the-future-of-language-modeling");
 const downloadButton = document.getElementById("download-button");
 const cheatsheetTitle = document.getElementById("cheatsheet-title");
 const pdfRegex = /\([A-Z/]\)/g;
+const feedbackForm = document.getElementById('feedback-form');
+const feedbackInput = document.getElementById('feedback-input');
 
 function convertLatexToPlainText(latexContent) {
   let plainText = latexContent
-      .replace(/\\textbf\{(.*?)\}/g, '**$1**') // Bold
-      .replace(/\\textit\{(.*?)\}/g, '*$1*')   // Italics
-      .replace(/\$\$(.*?)\$\$/g, '$1')         // Display Math
-      .replace(/\$(.*?)\$/g, '$1')             // Inline Math
-      .replace(/^## (.*$)/gim, '$1')           // Header
-      // Add more replacements here for other LaTeX commands as needed
-
+      // Remove bold and italics symbols
+      .replace(/[\*_]{2}(.*?)[\*_]{2}/g, '$1') // Bold and Italics
+      // Convert LaTeX equations to readable format
+      .replace(/\$\$(.*?)\$\$/g, 'Equation: $1') // Display Math
+      .replace(/\$(.*?)\$/g, '$1') // Inline Math
+      // Remove LaTeX specific commands
+      .replace(/\\textbf\{(.*?)\}/g, '$1') // Bold
+      .replace(/\\textit\{(.*?)\}/g, '$1') // Italics
+      .replace(/\\begin\{.*?\}(.*?)\\end\{.*?\}/gs, 'List: $1') // Lists
+      .replace(/\\item/g, ' -') // List items
+      .replace(/\\frac\{(.*?)\}\{(.*?)\}/g, '($1)/($2)') // Fractions
+      .replace(/\\left\[(.*?)\\right\]/g, '[$1]') // Brackets
+      .replace(/\\sqrt\{(.*?)\}/g, 'sqrt($1)') // Square root
+      .replace(/\\overline\{(.*?)\}/g, 'Mean of $1') // Overline (mean)
+      .replace(/\\underline\{(.*?)\}/g, '$1') // Underline
+      .replace(/_\\?{(.*?)}/g, ' subscript $1') // Convert subscripts
+      .replace(/\^\\?{(.*?)}/g, ' to the power of $1') // Convert superscripts
+      .replace(/\\text\{(.*?)\}/g, '$1') // Convert \text{} to plain text
+      .replace(/\\(?:right|left)/g, '') // Remove \right and \left
+      .replace(/\\[a-zA-Z]+\{(.*?)\}/g, '$1') // Generic command \command{content}
+      .replace(/\\[a-zA-Z]+/g, '') // Standalone LaTeX command \command
+      .replace(/[\$]{1,2}/g, '') // Remove $ symbols
+      // Remove Markdown heading symbols
+      .replace(/^#+\s+/gm, '') // Remove hash symbols used for headings
+      // Additional patterns can be added here
   return plainText;
 }
 
 
-async function generateCheatsheet() {
+
+async function generateCheatsheet(feedback = "") {
   let numPages = Math.max(1, Math.min(parseInt(inputPages.value, 10), 5));
   let topics = [];
 
@@ -54,7 +75,11 @@ async function generateCheatsheet() {
   if (inputMask.checked) topics.push ("the future of language modeling")
 
   let topicsString = topics.join(", ");
+  let userContent = `Create a ${numPages}-page cheatsheet for UCSD LIGN167: Deep Learning for Natural Language Understanding, covering these topics: ${topics.join(", ")}.`;
 
+  if (feedback) {
+    userContent += ` Feedback: ${feedback}`;
+  }
     const options = {
         method: "POST",
         headers: {
@@ -68,9 +93,20 @@ async function generateCheatsheet() {
                "topic of neural networks and deep learning, specifically relating to natural language" +
                "processing."
               },
-              {role: "user", content: `Create a ${numPages} pages cheatsheet covering the following topics: ${topicsString}. 
-              Include an equal mix of both computational questions with specific numbers and functions and conceptual questions. 
-              Remember to only generate ${numPages} pages.`}
+              {role: "user", content: `Assuming you are writing on a A4 size double space with 12 fonts size 'Times New Roman' font, create a ${numPages}-pages (set 1,000 words per each page.) cheatsheet for UCSD LIGN167: Deep Learning for Natural Language Understanding, 
+              covering these topics: ${topicsString}. Remember each page has to be at least 1,000 words.
+              The content should include clear and concise definitions and formulas, essential for exam preparation. 
+              Each formula and concept should be accompanied by step-by-step explanations to facilitate understanding.
+              
+              Key points and formulas should use all capital letters. Start a new line for every new topic, using line breaks for separation. 
+              Use bullet points (•) for listing items. Ensure the layout is clean, organized, and easy on the eyes, with ample spacing and clear section headings. 
+              Avoid using Latex formatting.
+
+              Include example problems with solutions to illustrate the practical application of the theories and formulas. 
+              Each topic should conclude with a summary section, recapping the most important points for quick revision.
+              
+              Focus on making the cheatsheet informative, student-friendly, and especially useful for exam preparation, with all information presented in a readable and visually appealing manner. 
+              Always end with a motivational message to do well on a exam!`}
             ]
         })
       };
@@ -84,16 +120,26 @@ async function generateCheatsheet() {
         // Convert LaTeX content to plain text
         const plainTextContent = convertLatexToPlainText(latexContent);
 
-        cheatSheetStatus.innerHTML = "Done!";
+        cheatSheetStatus.innerHTML = "Review and Provide Feedback!";
         cheatSheetResult.innerHTML = plainTextContent
+        document.getElementById('feedback-status').innerHTML = "Incorporated feedback! Feel free to suggest better feedback!";
         downloadButton.style.display = "block";
         cheatsheetTitle.style.display = "block";
+        feedbackForm.style.display = "block";
     }
     catch (error) {
         console.log(error);
     }
 }
 
+function handleFeedbackSubmit(event) {
+  event.preventDefault(); // Prevent page reload
+  const feedback = feedbackInput.value.trim();
+  if (feedback) {
+    document.getElementById('feedback-status').innerHTML = "Incorporating feedback and regenerating cheatsheet, please wait...";
+    generateCheatsheet(feedback);
+  }
+}
 function downloadPDF() {
   let currWindow = window.open("", "PRINT");
   let currSheet = document.getElementById("cheatsheet-result").innerHTML;
@@ -104,6 +150,6 @@ function downloadPDF() {
   currWindow.close();
   return true;
 }
-
 generateCheatSheetButton.addEventListener("click", generateCheatsheet);
+feedbackForm.addEventListener("submit", handleFeedbackSubmit);
 downloadButton.addEventListener("click", downloadPDF);
